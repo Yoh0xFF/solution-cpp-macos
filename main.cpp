830ab678c857c5d0127a87ca1169bc7c90049e58
Ioram Gordadze <ioram.gordadze@riptidehq.com>
2021-12-06 19:16:30 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <any>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  cout << "Hello World" << endl;
+  return 0;
+}
