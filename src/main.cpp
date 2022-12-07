04d448314b5a0b329c6c5029293020d93f58ea11
Ioram Gordadze <ioramgordadze@fb.com>
2022-12-07 12:23:15 +0000

    Muve includes to the separate header file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,4 @@
-#include <algorithm>
-#include <any>
-#include <array>
-#include <bitset>
-#include <functional>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
+#include "std.h"
 
 using namespace std;
 
---
+++ src/std.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <algorithm>
+#include <any>
+#include <array>
+#include <bitset>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
